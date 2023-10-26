4b09c55e99279d019f722459116d15560d23e6ff
Aditya Gor <adityagor13@gmail.com>
2023-10-26 18:33:59 +0530

    Resolved dependency in api

--- mern-chat/client/src/Chat.jsx
+++ mern-chat/client/src/Chat.jsx
@@ -23,7 +23,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket('ws://localhost:4040');
+    const ws = new WebSocket('ws://https://directsocialbackend.onrender.com');
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
